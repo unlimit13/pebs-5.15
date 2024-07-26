e4a1c6247f1e29e23ed5b4b878c4ec120c9e4138
hjcho <unlimit13@naver.com>
2024-07-26 15:14:31 +0000

    module set

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1613,5 +1613,7 @@ extern void __weak arch_perf_update_userpage(struct perf_event *event,
 #ifdef CONFIG_MMU
 extern __weak u64 arch_perf_get_page_size(struct mm_struct *mm, unsigned long addr);
 #endif
-
+extern int htmm__perf_event_init(struct perf_event *event, unsigned long nr_pages);
+extern int htmm__perf_event_open(struct perf_event_attr *attr_ptr, pid_t pid,
+	int cpu, int group_fd, unsigned long flags);
 #endif /* _LINUX_PERF_EVENT_H */
--- kernel/events/internal.h
+++ kernel/events/internal.h
@@ -242,5 +242,7 @@ static inline bool arch_perf_have_user_stack_dump(void)
 
 #define perf_user_stack_pointer(regs) 0
 #endif /* CONFIG_HAVE_PERF_USER_STACK_DUMP */
-
+int htmm__perf_event_init(struct perf_event *event, unsigned long nr_pages);
+int htmm__perf_event_open(struct perf_event_attr *attr_ptr, pid_t pid,
+	int cpu, int group_fd, unsigned long flags);
 #endif /* _KERNEL_EVENTS_INTERNAL_H */
